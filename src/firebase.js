a20ce98832fbb9f296d50c9de79717861d86346e
fendziorr <xfendi.channel@gmail.com>
2024-09-20 19:08:18 +0200

    remove firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,24 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// DOTENV variables for firebase
-const apiKey = process.env.FIREBASE_API_KEY;
-const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
-const projectId = process.env.FIREBASE_PROJECT_ID;
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
-const appId = process.env.FIREBASE_APP_ID;
-const measurementId = process.env.FIREBASE_MEASUREMENT_ID;
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: `${apiKey}`,
-  authDomain: `${authDomain}`,
-  projectId: `${projectId}`,
-  storageBucket: `${storageBucket}`,
-  messagingSenderId: `${messagingSenderId}`,
-  appId: `${appId}`,
-  measurementId: `${measurementId}`,
+  apiKey: "AIzaSyDOpi5g14aXbgY7KYgmnSem-ORYzJ1PWsw",
+  authDomain: "yolo-sitezy.firebaseapp.com",
+  projectId: "yolo-sitezy",
+  storageBucket: "yolo-sitezy.appspot.com",
+  messagingSenderId: "61567655316",
+  appId: "1:61567655316:web:15e005336dae69b1434381",
+  measurementId: "G-S2G94WK0KL"
 };
 
 // Initialize Firebase
